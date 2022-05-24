2af9b915a0a43ee50358b3f95e6e6d306a516104
Eliot <eliotpitman@gmail.com>
2022-05-24 01:57:08 -0500

    useStates for loading, data, error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-function App() {
-  const what = useState();
-  console.log(what);
-  return (
-    <>
-      <h1>hello</h1>
-    </>
-  );
+// https://api.github.com/users/eliot-pitman
+
+function App({ login }) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!login) return;
+    setLoading(true);
+    fetch(`https://api.github.com/users/${login}`)
+      .then((response) => response.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [login]);
+
+  if (loading) return <h1>loading...</h1>;
+
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+
+  if (!data) return null;
+
+  if (data) {
+    return (
+      <div>
+        <h1>{data.name}</h1>
+        <p>{data.location}</p>
+        <img alt={data.login} src={data.avatar_url} />
+      </div>
+    );
+  }
+
+  return <div>No users found</div>;
 }
 
 export default App;
